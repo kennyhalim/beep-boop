9cb8387f1cd6afff56be39507d1de153d88e6795
Kenny Halim <kennyhalim98@gmail.com>
2019-01-25 10:24:14 -0800

    add javascript code, separate business logic and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
+var beepBoop = function(number){
+  var finishedList = "";
+
+  for (var i = 0;i <= number; i++){
+    finishedList += i + ", ";
+  }
+
+  return finishedList;
+};
+
+
+
 
 $(document).ready(function(){
+  $("form#input").submit(function(event){
+
+    $("#result").empty();
+    var number = $("input#numberInput").val();
+
+    var result = beepBoop(number);
+
+    $("#boopResult").append(result);
+
+    event.preventDefault();
+  });
 
 });
